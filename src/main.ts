8135b40cdf1e357d2beb9d1101d4f21ef72af7d8
santiagogape <santiago.galindo101@alu.ulpgc.es>
2026-03-17 18:07:10 +0000

    vuetify and mdi corrections

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
 import App from './App.vue'
 import router from './router'
+import { vuetify } from './plugings/vuetify'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,10 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/dev',
+      component: () => import('../views/PlaygroundView.vue'),
+    },
   ],
 })
 
