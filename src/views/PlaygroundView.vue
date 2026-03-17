55d0401e153b71a4dc3c0fb40ad446bd9d60167c
santiagogape <santiago.galindo101@alu.ulpgc.es>
2026-03-17 19:34:17 +0000

    incluyo el formulario en PlaygroundView

--- src/views/PlaygroundView.vue
+++ src/views/PlaygroundView.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import NavBar from '@/components/testing/NavBar.vue'
 import TheFooter from '@/components/testing/TheFooter.vue'
+import NewProjectForm from '@/components/NewProjectForm.vue'
 </script>
 
 <template>
   <v-app>
     <NavBar />
     <v-container> </v-container>
+    <NewProjectForm />
     <TheFooter />
   </v-app>
 </template>
